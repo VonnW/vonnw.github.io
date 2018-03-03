869bcd34ed2f855c2396d1389b34c5e99ea1ba8e
VonnW <vonn.wheatley@gmail.com>
2018-03-02 19:18:45 -0500

    fixed everything, organized it too

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("load", function() {
+	clean(document.getElementsByTagName("header")[0]);
+}
+
+// From sitepoint.com/removing-useless-nodes-from-the-dom/
+// I just fixed their awful formatting
+function clean(node) {
+	for (var n = 0; n < node.childNodes.length; n++) {
+		var child = node.childNodes[n];
+		
+		if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+			node.removeChild(child);
+			n--;
+		} else if (child.nodeType === 1) {
+			clean(child);
+		}
+	}
+}
